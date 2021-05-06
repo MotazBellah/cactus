65112e87ffdff8b62a1556d91932bfe32be25f3c
MotazBellah <engineer.android@yahoo.com>
2021-05-06 23:41:48 +0200

    Display the curve based on selection

--- cactusApp/static/js/app.js
+++ cactusApp/static/js/app.js
@@ -1,1 +1,27 @@
-// alert('H');
+$('#bmi-img').hide()
+$("#weight").css("color", "green");
+
+
+$('#weight').click(function(){
+
+    $("#weight").css("color", "green");
+    $("#bmi").css("color", "black");
+    $( "#bmi-img" ).hide( "slow", function() {
+        alert( "Animation complete." );
+      });
+
+    $('#weight-img').show(1000)
+
+});
+
+
+$('#bmi').click(function(){
+
+    $("#bmi").css("color", "green");
+    $("#weight").css("color", "black");
+    $( "#weight-img" ).hide( "slow", function() {
+        alert( "Animation complete." );
+      });
+    $('#bmi-img').show(1000)
+
+});
