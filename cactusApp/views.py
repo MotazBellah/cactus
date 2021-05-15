890da8bc11895365ac29738713629c55dac567e1
MotazBellah <engineer.android@yahoo.com>
2021-05-15 14:07:37 +0200

    Update requirements

--- cactusApp/views.py
+++ cactusApp/views.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Child, Measurement
 from django.contrib.auth import login, logout, authenticate
-from pygrowup import Calculator
 from .getData import draw, getData
 from pygrowup import helpers
 import datetime
@@ -226,49 +225,6 @@ def delete_child(request):
 
     return HttpResponseRedirect(reverse("kids"))
 
-
-
-def calculate_score(request):
-    '''Index view, render the home page'''
-    if request.method == "POST":
-        weight = request.POST['weight']
-        height = request.POST['height']
-        head_circumference = request.POST['HeadCircumference']
-
-        # print("////////////////////")
-        # print(weight)
-        # print(height)
-        # print(head_circumference)
-        calculator = Calculator(adjust_height_data=False, adjust_weight_scores=False,
-                       include_cdc=False, logger_name='pygrowup',
-                       log_level='INFO')
-
-        great_day = datetime.datetime.utcnow().date()
-        nine_months_ago = great_day - datetime.timedelta(days=(24 * 30.4374))
-        dob = nine_months_ago.strftime("%d%m%y")
-
-        my_child = {'date_of_birth' : dob, 'sex' : 'male', 'weight' : '8.0', 'height' : '69.5'}
-
-        # transform 'male' into 'M'
-        valid_gender = helpers.get_good_sex('male')
-
-        # transform something like '100309' into '2009-03-10'
-        valid_date = helpers.get_good_date(my_child['date_of_birth'])
-
-        # calculate 9 months from valid_date
-        valid_age = helpers.date_to_age_in_months(valid_date[1])
-
-        # calculate weight-for-age zscore
-        wfa_zscore_for_my_child = calculator.wfa(my_child['weight'], valid_age, valid_gender)
-
-        # calculate weight-for-age zscore
-        bmi = calculator.bmifa(my_child['weight'], valid_age, valid_gender)
-        # print(calculator.bmifa.__defaults__)
-
-
-        print(wfa_zscore_for_my_child, bmi)
-
-    return render(request, 'cactusApp/index.html')
 
 
 # def chart_js(request, kid_id):
